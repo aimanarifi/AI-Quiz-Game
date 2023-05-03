74133bd9e874e203cc1bc26d73575e701397ff2d
Muhammad Aiman Arifi <arifikamal01@gmail.com>
2023-05-03 22:35:00 +0100

    Quiz page string auto formatting and font change

--- production/general/quiz.py
+++ production/general/quiz.py
@@ -11,20 +11,105 @@ import pygame
 from typing import Optional
 pygame.init()
 
+class MultilineText():
+
+    """
+    Formatting algo 
+    1) check maximum char per text
+    2) get a string
+    3) slice them into a list of words
+    4) make a new list representing lines
+    5) iterate over list of word, append a row in the list of lines if len(current line) < maximum char else append the next line
+    6) get the text wr
+    7) set the text rects relative to the text wrapper
+    """
+
+    def __init__(self, text: str, max_char: int=30, font: pygame.font.Font= None, color: str="Black"):
+
+        self.MAX_CHAR_PER_LINE = max_char
+
+        #set wrappers object to None, it'll be updated later based on the text
+        self.wrapper_surf = None  
+        self.wrapper_rect = None
+
+        if font:
+            self.font = font
+
+        self.color = color
+        self.surfs = []
+        self.rects = []
+
+        self.raw_text = text
+        self.words = self.raw_text.split(" ")
+        self.formatted_text = [[]]
+
+        self.process_text()
+
+    def process_text(self):
+
+        """
+        Process the raw text, and update the surfs and rects
+        the wrapper also will automatically resized to fit the texts
+        """
+
+        line_index = 0
+
+        for word in self.words:
+
+            current_len = sum([ len(word) + 1 for word in self.formatted_text[line_index]])
+
+            if len(word) + current_len + 1 < self.MAX_CHAR_PER_LINE: #add 1 space to indicate extra spacing
+                #in the same line
+                self.formatted_text[line_index].append(word)
+
+            else:
+
+                #move to the new line
+                self.formatted_text.append([word])
+                line_index += 1
+
+
+        #format them in to a string instead of list of words, and update the txt and wrapper surfs and rects
+        temp_y = 0
+        temp_width = 0
+
+        
+        for i, line in enumerate(self.formatted_text):
+
+            self.formatted_text[i] = " ".join(line)                                        #turn into 1 string
+            self.surfs.append(self.font.render(self.formatted_text[i], False, self.color)) #make the txt surf of that string
+
+            temp_y = pygame.font.Font.get_linesize(self.font)*i + 4*i
+
+            self.rects.append(self.surfs[i].get_rect(topleft = (0, temp_y)))               #make the txt rect of that txt surf
+            
+            if temp_width < self.surfs[i].get_size()[0]:
+                temp_width = self.surfs[i].get_size()[0]
+
+        self.wrapper_surf = pygame.Surface((temp_width, self.rects[len(self.rects)-1].bottom)).convert_alpha()
+        self.wrapper_surf.fill((0,0,0,0)) #wrapper surf should be transparent
+        self.wrapper_rect = self.wrapper_surf.get_rect()
+
+    def display(self):
+
+        for txt_surf, txt_rect in zip(self.surfs, self.rects ):
+
+            self.wrapper_surf.blit(txt_surf, txt_rect)
+
 class Quiz:
 
     #Essential graphics object
     screen = None
     background,background_rect = None, None
     question_bg_surf, question_bg_rect = None, None
-    question_surf, question_rect = None, None
+    question, question_rect = None, None
     button_txt, button_txt_rect = None, None 
     submit_button , submit_button_rect = None, None
 
-    FONT_PATH = "graphics/font/PeaberryBase.ttf"
-    question_font = pygame.font.Font(FONT_PATH, 32)
-    button_font = pygame.font.Font(FONT_PATH, 24)
-
+    FONT_PATH = "graphics/font/monogram.ttf"
+    question_font = pygame.font.Font(FONT_PATH, 34)
+    button_font = pygame.font.Font(FONT_PATH, 40)
+    
     CLOUD_CROSS = pygame.transform.scale_by(pygame.image.load("graphics/art/cloud_cross.png"),4)
     CLOUD_CIRCLE = pygame.transform.scale_by(pygame.image.load("graphics/art/cloud_circle.png"),4)
     CLOUD_TICK = pygame.transform.scale_by(pygame.image.load("graphics/art/cloud_tick.png"),4)
@@ -35,15 +120,19 @@ class Quiz:
     on_click = False
 
     class Option(pygame.sprite.Sprite):
+
         option_font = None
+
         def __init__(self, txt: str, is_answer: bool, quiz: Optional[any] = None):
             super().__init__()
+
             self.quiz = quiz
-            self.option_font = pygame.font.Font(self.quiz.FONT_PATH, 20) if self.quiz  else pygame.font.Font(pygame.font.get_default_font(), 20)
+            self.option_font = pygame.font.Font(self.quiz.FONT_PATH, 30) if self.quiz  else pygame.font.Font(pygame.font.get_default_font(), 20)
             self.image = pygame.transform.scale_by(pygame.image.load("graphics/art/UI/beige_rectangle_2x7.png"),4.5)
             self.rect = self.image.get_rect()
-            self.txt = self.option_font.render(txt, False, 'Black')
-            self.txt_rect = self.txt.get_rect(center=self.rect.center)
+            self.txt = MultilineText(txt, 40, self.option_font, "Black")
+            self.txt_rect = self.txt.wrapper_rect
+    
             self.is_answer = is_answer
 
         def set_position(self, sides: list[int]):
@@ -55,6 +144,12 @@ class Quiz:
             self.txt_rect.center = self.rect.center
 
         def update(self, quiz):
+
+
+            self.quiz.screen.blit(self.image, self.rect)               #blit option banner bg
+            self.txt.display()                                         #blit the lines of text into the wrapper
+            self.quiz.screen.blit(self.txt.wrapper_surf,self.txt_rect) #blit the wrapper into the screen
+
             #click effect
             if self == quiz.selected_option:
                 self.quiz.screen.blit(Quiz.CLOUD_CIRCLE, (self.rect.right - 64 -16,self.rect.top + 32))
@@ -64,8 +159,6 @@ class Quiz:
                     self.quiz.screen.blit(Quiz.CLOUD_TICK, (self.rect.right - 64 -16,self.rect.top + 32))
                 if self == quiz.selected_option and not self.is_answer:
                     self.quiz.screen.blit(Quiz.CLOUD_CROSS, (self.rect.right - 64 -16,self.rect.top + 32))
-
-            self.quiz.screen.blit(self.txt,self.txt_rect)
 
     def __init__(self, question: str, choice: list[str], answer: str):
 
@@ -78,13 +171,20 @@ class Quiz:
         self.selected_option = None
 
     def display(self):
-
+        """
+        Blit or draw everything
+        """
         self.screen.blit(self.background, self.background_rect)
         self.screen.blit(self.question_bg_surf,self.question_bg_rect)
-        self.screen.blit(self.question_surf, self.question_rect)
+        
+        #Blit the text wrapper into the screen, blit the text into the wrapper using 
+        self.screen.blit(self.question.wrapper_surf, self.question_rect)
+        self.question.display()
+
         self.screen.blit(self.submit_button, self.submit_button_rect)
         self.screen.blit(self.button_txt, self.button_txt_rect)
-        self.options_group.draw(self.screen)
+
+        #objects in option group is drawn in update function
         self.options_group.update(self)
     
     def setup(self):
@@ -95,12 +195,14 @@ class Quiz:
         self.screen = pygame.display.get_surface()
         self.background = pygame.transform.scale_by(pygame.image.load("graphics/art/bg_1.png"),2)
         self.background_rect = self.background.get_rect()
-        #self.background.fill('#262b44')
+
         #Set the question
         self.question_bg_surf = pygame.transform.scale_by(pygame.image.load("graphics/art/UI/brown_rectangle_3x14.png"),4.5)
         self.question_bg_rect = self.question_bg_surf.get_rect(midtop=(self.screen.get_width()/2,64))
-        self.question_surf = self.question_font.render(self.question,False , "White")
-        self.question_rect = self.question_surf.get_rect(center=(self.question_bg_rect.center))
+        self.question = MultilineText(self.question, 70, self.question_font, "White")
+        self.question_rect = self.question.wrapper_rect
+        self.question_rect.center = self.question_bg_rect.center
+
         #set the answer options
         REF_POINT = (self.screen.get_width()/2 , self.screen.get_height()*3/4-50) # the center between four option blocks (x,y)
         for index, opt in enumerate(self.options):
@@ -191,15 +293,13 @@ class Quiz:
             #animate background
             inc = -1 if self.background_rect.left == 0 else inc
             inc = 1 if self.background_rect.right == self.screen.get_width() else inc
-            if not counter % 2:
-                self.background_rect.left += inc    
+
+            if not counter % 2: self.background_rect.left += inc    
             
             self.display()
 
             pygame.display.update()
             self.clock.tick(60)
-
-        return True
 
     def get_score(self):
 
@@ -214,11 +314,32 @@ if __name__ == "__main__":
     pygame.display.set_caption("Quiz page")
 
     #create quiz object
-    q1 = Quiz("This is the prompt", ["AAAA","BBBB","CCCC","DDDD"], "CCCC")
+    test_question ="One of the challenges of a multi-cloud approach is that different cloud solutions run in different software environments. Organizations want to build applications that can easily move across a wide range of these environments without creating integration difficulties. Which of the following helps mitigate such challenges?"
+    test_options = ["Use private and public cloud in your business", "Use VMware services", "Use Container technologies","All of them"]
+    q1 = Quiz(test_question, test_options, "Use Container technologies")
 
     #run the page
     q1.run()
 
     #get score
-    print(q1.get_score())
+    #print(q1.get_score())
+
+    # test_text = "asd asdsdf gfs ag r sdfgsd gr sdf gar g sdfg sdfg r g dsfgdsfg dfs sdfg sksfdgjnsd jvuubrue sdfugu isdfgi kndsfgi ads dadsf a df sdddf sn "
+    # mt = MultilineText( test_text, screen.get_rect(), 50, pygame.font.Font("graphics/font/PeaberryBase.ttf",24), "White")
+
+    # while True:
+    
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             pygame.quit()
+    #             exit
+
+    #     for i in range(len(mt.formatted_text)):
+    #         screen.blit(mt.surfs[i], mt.rects[i])
+        
+    #     pygame.display.update()
+
+
+
+    
 
